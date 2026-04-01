6a7ac4c434ffea8025bfa09c7da8066a2b3c5d49
Aritra Hutait <154417739+Aritrahutait07@users.noreply.github.com>
2026-04-02 00:46:03 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> curr = new  ArrayList<>();
+        return backtrack(nums,curr,res,0);
+    }
+    public List<List<Integer>> backtrack(int[] nums,List<Integer> currpath,List<List<Integer>> result,int index){
+        result.add(new ArrayList<>(currpath));
+        // Explore all the choice
+        for(int i=index;i<nums.length;i++){
+            // choose
+            currpath.add(nums[i]);
+            // explore
+            backtrack(nums,currpath,result,i+1);
+            // unchoose or backtrack
+            currpath.remove(currpath.size()-1);
+        }
+        return result;
+    }
+}
