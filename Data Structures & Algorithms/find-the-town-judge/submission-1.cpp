4f376f85b07747d9a741ebe1417a1536d59f2ab3
Aritra Hutait <154417739+Aritrahutait07@users.noreply.github.com>
2026-06-12 13:51:20 +0530

    Add: find-the-town-judge - submission-1

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> trustScores (n+1,0);
+        for(auto& edge:trust){
+            int a = edge[0];
+            int b = edge[1];
+
+            trustScores[a]--;
+            trustScores[b]++;
+        }
+        for(int i=1;i<=n;i++){
+            if (trustScores[i] == n - 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+};
